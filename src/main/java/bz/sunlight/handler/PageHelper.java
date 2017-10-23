361c473b2f1ffe9ce8d89b36090e10008249e87a
时孝丛 <shixiaocong@sunlight.bz>
2017-10-23 16:57:27 +0800

    按照Checkstyle规则填加Javadoc

--- src/main/java/bz/sunlight/handler/PageHelper.java
+++ src/main/java/bz/sunlight/handler/PageHelper.java
@@ -1,6 +1,13 @@
 package bz.sunlight.handler;
 
 public class PageHelper {
+  /**
+   * 根据pageIndex,pageSize计算数据Offset.
+   *
+   * @param pageIndex.
+   * @param pageSize.
+   * @return Integer
+   */
   public static Integer toOffset(Integer pageIndex, Integer pageSize) {
     int pageNo = pageIndex == null ? 1 : pageIndex;
     pageNo = Math.max(pageNo - 1, 0);
