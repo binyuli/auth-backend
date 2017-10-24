a22b3bf8a045bc0e299f1cc9835c758e7e9fa444
李彬瑜 <libinyu@sunlight.bz>
2017-10-24 18:08:09 +0800

    调整分页用的工具类

--- src/main/java/bz/sunlight/handler/PageHelper.java
+++ src/main/java/bz/sunlight/handler/PageHelper.java
@@ -9,9 +9,32 @@ public class PageHelper {
    * @return Integer
    */
   public static Integer toOffset(Integer pageIndex, Integer pageSize) {
-    int pageNo = pageIndex == null ? 1 : pageIndex;
-    pageNo = Math.max(pageNo - 1, 0);
-    int offset = pageNo * pageSize;
-    return offset;
+    if (pageSize != null) {
+      int pageNo = pageIndex == null ? 1 : pageIndex;
+      pageNo = Math.max(pageNo - 1, 0);
+      int offset = pageNo * pageSize;
+      return offset;
+    } else {
+      return null;
+    }
   }
+
+  /**
+   * 获取页面总数
+   *
+   * @param totalItemCount 数据总数.
+   * @param pageSize 每页展示数量.
+   */
+  public static Integer getTotalPage(Integer totalItemCount, Integer pageSize) {
+    if (pageSize != null) {
+      if (totalItemCount % pageSize == 0) {
+        return totalItemCount / pageSize;
+      } else {
+        return totalItemCount / pageSize + 1;
+      }
+    } else {
+      return null;
+    }
+  }
+
 }
