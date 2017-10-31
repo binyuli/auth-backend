550888affb79e233aa93cf14b42dc7716146c7b3
李彬瑜 <libinyu@sunlight.bz>
2017-10-31 18:56:30 +0800

    Revert "修改用户——增加一个单元测试"
    This reverts commit 8e8a59f6e0606dcf92a3af1554f6ba4460f89dc3.

---
+++ src/test/java/bz/sunlight/service/ServiceApplicationTests.java
@@ -0,0 +1,23 @@
+package bz.sunlight.service;
+
+import java.util.List;
+
+import bz.sunlight.dao.ApiMapper;
+import bz.sunlight.dao.PageMapper;
+import bz.sunlight.entity.Page;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceApplicationTests {
+
+  @Test
+  public void contextLoads() {
+
+  }
+
+}
