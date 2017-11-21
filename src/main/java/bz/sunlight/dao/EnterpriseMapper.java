31d9ef342fc34e93aa94c406101a438a2f77e795
时孝丛 <shixiaocong@sunlight.bz>
2017-11-21 11:43:50 +0800

    增加企业相关Entity Mapper

---
+++ src/main/java/bz/sunlight/dao/EnterpriseMapper.java
@@ -0,0 +1,8 @@
+package bz.sunlight.dao;
+
+import bz.sunlight.BaseMapper;
+import bz.sunlight.entity.Enterprise;
+import bz.sunlight.entity.EnterpriseExample;
+
+public interface EnterpriseMapper extends BaseMapper<Enterprise, EnterpriseExample, String> {
+}
