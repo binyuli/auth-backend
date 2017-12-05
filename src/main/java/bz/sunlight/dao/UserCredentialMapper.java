64f5da438a004909daffb0174755dd1195b0d502
马振飞 <mazhenfei@sunlight.bz>
2017-12-05 13:21:52 +0800

    根据数据库变更调整dao,entity,mapper

--- src/main/java/bz/sunlight/dao/UserCredentialMapper.java
+++ src/main/java/bz/sunlight/dao/UserCredentialMapper.java
@@ -1,9 +1,11 @@
 package bz.sunlight.dao;
 
+import bz.sunlight.BaseMapper;
 import bz.sunlight.entity.UserCredential;
+import bz.sunlight.entity.UserCredentialExample;
 import org.apache.ibatis.annotations.Param;
 
-public interface UserCredentialMapper {
+public interface UserCredentialMapper extends BaseMapper<UserCredential, UserCredentialExample, String> {
   UserCredential findByEnterpriseCodeAndUsername(@Param("enterpriseCode") String enterpriseCode,
                                                  @Param("username") String username);
 }
