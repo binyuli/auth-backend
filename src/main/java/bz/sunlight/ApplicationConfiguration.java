aa11be68655993af9f151ebbb7392c4940b09db8
吕楠 <lvnan@sunlight.bz>
2017-12-20 10:53:30 +0800

    增加 OpenTracing 跟踪器，可跟踪 Web

--- src/main/java/bz/sunlight/ApplicationConfiguration.java
+++ src/main/java/bz/sunlight/ApplicationConfiguration.java
@@ -16,6 +16,9 @@ public class ApplicationConfiguration {
   @Value("${security.oauth2.resource.jwt.key-value}")
   public String jwtKeyValue;
 
+  @Value("${spring.application.name}")
+  public String applicationName;
+
   /**
    * 从配置文件中加载签名的密钥，并初始化 Token 服务.
    *
@@ -25,5 +28,17 @@ public class ApplicationConfiguration {
   public DefaultTokenService tokenService() {
     SecretKeySpec key = new SecretKeySpec(jwtKeyValue.getBytes(), SignatureAlgorithm.HS256.getJcaName());
     return new DefaultTokenService(key);
+  }
+
+  /**
+   * 构造 OpenTracing 跟踪器.
+   *
+   * @return 跟踪器实例
+   */
+  @Bean
+  public io.opentracing.Tracer jaegerTracer() {
+    return new com.uber.jaeger.Configuration(applicationName,
+        com.uber.jaeger.Configuration.SamplerConfiguration.fromEnv(),
+        com.uber.jaeger.Configuration.ReporterConfiguration.fromEnv()).getTracer();
   }
 }
